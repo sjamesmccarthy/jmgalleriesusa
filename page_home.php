e6d22c7b4a3eed350f72a4a9c71892a0b30dde1d
James McCarthy <hikerbikerwriter@gmail.com>
2019-02-26 21:28:46 -0800

    Added homepage template w/demo links

---
+++ page_home.php
@@ -0,0 +1,5 @@
+<P>WELCOME TO THE HOME PAGE</P>
+
+<ul>
+    <li><a href="/oceans-lakes-waterfalls/">Oceans, Lakes & Waterfalls</a></li>
+    <li><a href="/oceans-lakes-waterfalls/photo/once-afloat">Once Afloat Detail Page</a></li>
