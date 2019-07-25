5826b67d673e74cbefe77b22cec609b11962eadd
James McCarthy <hikerbikerwriter@gmail.com>
2019-07-25 16:08:09 -0700

    updated inline css for testing

--- view/view_catalog.php
+++ view/view_catalog.php
@@ -6,8 +6,9 @@
         $file_name = '/catalog' . '/__thumbnail/' . $value['file_name'] . '.jpg';
 
         if(file_exists( $_SERVER['DOCUMENT_ROOT'] . $file_name )) {
+            $p_thumbs .= '<li style="box-sizing: border-box; display: inline-block; height: 240px; overflow: hidden; margin-bottom: -4px;">';
             $p_thumbs .= '<a href="' . $this->page->catalog . '/' . $value['file_name'] . '">';
-            $p_thumbs .= '<img style="height: 25%" src="'. $file_name . '" />';
+            $p_thumbs .= '<img style="width: 100%" src="'. $file_name . '" />';
             $p_thumbs .= '</a></li>';
         } else {
             $p_thumbs .= "";
@@ -19,5 +20,8 @@
 
 <h1 style="text-align: center; padding-top: 40px"><?= $this->page->title ?></h1>
 
-<?= $p_thumbs ?>
-<p style="padding: 20px;"><a href="/">Back To Home</a></p>
+    <ul style="width: 95%;">
+    <?= $p_thumbs ?>
+    </ul>
+
+<p style="padding: 20px; clear: both;"><a href="/">Back To Home</a></p>
