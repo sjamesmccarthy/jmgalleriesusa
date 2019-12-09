12d23adb45a5b7d8d64b47b7a22e574103a1699e
James McCarthy <hikerbikerwriter@gmail.com>
2019-12-09 08:12:04 -0800

    remvoed href target on data table listing in catalog view

--- view/view_admin_catalog.php
+++ view/view_admin_catalog.php
@@ -41,7 +41,7 @@
                 { data: 'title',
                     "render": function(data, type, row, meta){
                         if(type === 'display'){
-                            data = '<a target="_detail" href="/studio/catalog-add?id=' + row.file_name + '">' + data + '</a>';
+                            data = '<a href="/studio/catalog-add?id=' + row.file_name + '">' + data + '</a>';
                         }  
                         return data;
                     } 
