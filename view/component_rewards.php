64fa2f7bb07f10bd6784ab01232725e192af8322
sjamesmccarthy <hikerbikerwriter@gmail.com>
2020-04-17 13:27:43 -0700

    bug fix for promo code

--- view/component_rewards.php
+++ view/component_rewards.php
@@ -12,6 +12,7 @@ version: 1
 
 $myrewards_points = $this->api_MyRewards_Get_Points($this->collector_data_obj->collector_id);
 $myrewards_points = round($myrewards_points['points']*1.5);
+$p_code = strtoupper($this->collector_data_obj->last_name) . $this->collector_data_obj->collector_id;
 
 /* GENERATE HTML BLOCK */
 if ($this->config->components['polarized'] == 'true') {  
@@ -21,7 +22,7 @@ $html = <<< END
             <div class="most-popular--title col-12">
             <h2 class="uppercase ">YOUR REWARDS = $myrewards_points  POINTS</h2>
             <p><b>As a collector you are enrolled in our rewards program and it's pretty simple.</b></p>
-            <p class="mt-8">To get started share our website with your family and friends, and if they purchase a Limited Edition Fine Art Photograph, we will send you a tinyViews&trade; of your choice. You also receive points, and once you reach 2,500 points you're getting a free 16x20 Limited Edition of your selection. <u>Simply share this promo-code: strtoupper($res_first_name) $res_collector_id ,</u> and your friends and family will use this code when ordering their fine art either online, by phone or email. They will also receive a 15% #itswhoyouknow discount.</p>
+            <p class="mt-8">To get started share our website with your family and friends, and if they purchase a Limited Edition Fine Art Photograph, we will send you a tinyViews&trade; of your choice. You also receive points, and once you reach 2,500 points you're getting a free 16x20 Limited Edition of your selection. <u>Simply share this promo-code: {$p_code} ,</u> and your friends and family will use this code when ordering their fine art either online, by phone or email. They will also receive a 15% #itswhoyouknow discount.</p>
             </div>
             
             <div class="col-12">
@@ -32,7 +33,7 @@ $html = <<< END
                     <input type="hidden" id="formType" name="formType" value="referrCollectorForm" />
                     <input type="hidden" id="referred_by" name="referred_by" value="{$this->collector_data_obj->first_name} {$this->collector_data_obj->last_name}" />
                     <input type="hidden" id="referred_by_email" name="referred_by_email" value="{$this->collector_data_obj->email}" />
-                    <input type="hidden" id="promo_code" name="promo_code" value="strtoupper({$this->collector_data_obj->last_name}) {$this->collector_data_obj->collector_id} " />
+                    <input type="hidden" id="promo_code" name="promo_code" value="{$p_code} " />
                     <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" />
                     <input name="refer_IP" type="hidden" value="{$_SERVER['REMOTE_ADDR']} " />
 
