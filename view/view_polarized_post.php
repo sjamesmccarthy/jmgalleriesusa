966fba821ef3641d08cbbba21fef9cefe2ee1735
sjamesmccarthy <hikerbikerwriter@gmail.com>
2020-08-21 08:23:43 -0700

    misc changes

--- view/view_polarized_post.php
+++ view/view_polarized_post.php
@@ -77,12 +77,12 @@
 
                 <div style="display: inline-block; margin-right: 2rem;">
                     <a class="response--icon-cheers" href="#"><i class="fas fa-glass-cheers"></i></a>
-                    <p style="display:inline-block; vertical-align: top; margin-left: .5rem; margin-top: 7px; font-size: .8rem;"><span class="repsonse--cheers-count"><?= $res_cheers ?></span> Cheers!</p>
+                    <p class="response--icon-label"><span class="repsonse--cheers-count"><?= $res_cheers ?></span> Cheers!</p>
                 </div>
 
                 <div style="display: inline-block">
                     <a class="response--icon response--icon-bubble" href=""><i class="fas fa-comment-alt"></i></a>
-                    <p class="response--icon--count" style="display:inline-block; vertical-align: top; margin-left: .5rem; margin-top: 7px; font-size: .8rem;"><?= $res_responses ?></p>
+                    <p class="response--icon--count response--icon-label"><?= $res_responses ?></p>
                 </div>
 
                 <div style="display: inline-block; float: right;">
