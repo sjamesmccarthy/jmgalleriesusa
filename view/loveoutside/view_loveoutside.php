dd9fdaf94e6c2c61f4a12ad8550c443b66b0e592
James McCarthy <james@jmgalleries.com>
2022-05-10 08:05:47 -0700

    loveoutside content update

--- view/loveoutside/view_loveoutside.php
+++ view/loveoutside/view_loveoutside.php
@@ -26,8 +26,7 @@
         
         <p>Next meeting is Saturday, June 4 in Carson City at Comma Coffee around 10am. We will meet up for an intro to the group and who we are, then will be taking a short field-trip up Prison Hill for some amazing vistas of the Carson Valley. <i>If you're interested, just show up, and, like the sunshine everything is free &mdash; except for the delicious Comma Coffee edibles.</i></p>
 
-
-        <p>Our Q2 conservation goal is trail trash clean-up along the Carson river from Buzzy's Ranch Trailhead (Carson River Park)  north along the Carson River to where it intersects with the Mexican Ditch Trail at the golfcourse. Join us on Saturday, June 25 at 9AM at the Carson River Park Parking lot. Please bring your supplies to pick up trash. It should be an easy walk for about 2 hours. Even though we will be picking up trash don't forget your camera - spring has sprung along the river!</p>
+        <!-- <p>Our Q2 conservation goal is trail trash clean-up along the Carson river from Buzzy's Ranch Trailhead (Carson River Park)  north along the Carson River to where it intersects with the Mexican Ditch Trail at the golfcourse. Join us on Saturday, June 25 at 9AM at the Carson River Park Parking lot. Please bring your supplies to pick up trash. It should be an easy walk for about 2 hours. Even though we will be picking up trash don't forget your camera - spring has sprung along the river!</p> -->
 
         <h3 class="pt-32">Start Your Own Group</h3>
         <p>If you have more than 5 people interested in the outdoors +photography whom are also passionate about community in their area <a href="/contact">drop me a note</a> to learn how you can create your loveOutside.Org group and be listed in the upcoming directory.</p>
