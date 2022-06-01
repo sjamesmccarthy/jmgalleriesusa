8ff4fea02291eaa6a3d308485329121c7c8c57d0
James McCarthy <james@jmgalleries.com>
2022-06-01 07:24:28 -0700

    update to LO.org content, added static index for maintenance mode

--- view/loveoutside/view_loveoutside.php
+++ view/loveoutside/view_loveoutside.php
@@ -20,23 +20,25 @@
 
         <div class="divider"></div>
 
-        <h3>The Carson/Tahoe Group</h3>
-        <p>As well as Reno, Sparks, Gardnerville, Minden, Dayton, Incline Village and other surrounding communities. </p>
-        <p>Currently meeting every other month in one of the surrounding communities to discuss all things outdoors and photography as well as go on a short outdoor +photography related field-trip in the area.</p>
+        <!-- <h3>The Carson/Tahoe Group</h3> -->
+        <!-- <p>As well as Reno, Sparks, Gardnerville, Minden, Dayton, Incline Village and other surrounding communities. </p> -->
+        <!-- <p>Currently meeting every other month in one of the surrounding communities to discuss all things outdoors and photography as well as go on a short outdoor +photography related field-trip in the area.</p> -->
         
-        <p>Next meeting is Saturday, June 25 in Carson City at Comma Coffee around 10am. We will meet up for an intro to the group and who we are, then will be taking a short field-trip up <a target="_new" href="https://visitcarsoncity.com/attractions/prison-hill-north-loop-trail/">Prison Hill - West Loop (click here for more information)</a> for some amazing vistas of the Carson Valley. <i>If you're interested, just show up, and, like the sunshine everything is free &mdash; except for the delicious Comma Coffee edibles.</i></p>
+        <!-- <p>Next meeting is Saturday, June 25 in Carson City at Comma Coffee around 10am. We will meet up for an intro to the group and who we are, then will be taking a short field-trip up <a target="_new" href="https://visitcarsoncity.com/attractions/prison-hill-north-loop-trail/">Prison Hill - West Loop (click here for more information)</a> for some amazing vistas of the Carson Valley. <i>If you're interested, just show up, and, like the sunshine everything is free &mdash; except for the delicious Comma Coffee edibles.</i></p> -->
 
         <!-- <p>Our Q2 conservation goal is trail trash clean-up along the Carson river from Buzzy's Ranch Trailhead (Carson River Park)  north along the Carson River to where it intersects with the Mexican Ditch Trail at the golfcourse. Join us on Saturday, June 25 at 9AM at the Carson River Park Parking lot. Please bring your supplies to pick up trash. It should be an easy walk for about 2 hours. Even though we will be picking up trash don't forget your camera - spring has sprung along the river!</p> -->
 
-        <h3 class="pt-32">Start Your Own Group</h3>
-        <p>If you have more than 5 people interested in the outdoors +photography whom are also passionate about community in their area <a href="/contact">drop me a note</a> to learn how you can create your loveOutside.Org group and be listed in the upcoming directory.</p>
+        <!-- <h3 class="pt-32">Start Your Own Group</h3> -->
+        <!-- <p>If you have more than 5 people interested in the outdoors +photography whom are also passionate about community in their area <a href="/contact">drop me a note</a> to learn how you can create your loveOutside.Org group and be listed in the upcoming directory.</p> -->
 
-        <h3 class="nomar-top mb-16 pt-32">Sign Up For The First Friday Email</h3>
-        <?php $this->getPartial('newsletter'); ?>
+        <!-- <h3 class="nomar-top mb-16 pt-32">Sign Up For The First Friday Email</h3> -->
+        <?php 
+            // $this->getPartial('newsletter'); 
+        ?>
 
-        <p class="mt-32">
-        <a target="_new" class="mr-16 blue" style="font-size: 1.5rem" href="http://twitter.com/loveoutsideorg"><i class="fab fa-twitter"></i></a>
-        </p>
+        <!-- <p class="mt-32"> -->
+        <!-- <a target="_new" class="mr-16 blue" style="font-size: 1.5rem" href="http://twitter.com/loveoutsideorg"><i class="fab fa-twitter"></i></a> -->
+        <!-- </p> -->
 
         </div>
     </div>
