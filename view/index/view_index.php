f00fe8b1f5e233eced4f125a5466f04c15a1f400
James McCarthy <james@jmgalleries.com>
2022-08-29 10:35:18 -0700

    added scss file for index and adjusted mobile styles

--- view/index/view_index.php
+++ view/index/view_index.php
@@ -1,35 +1,20 @@
-<style>
-
-    .v-align-m {
-        position: absolute;
-        top : 50%;
-        left: 50%;
-        -ms-transform: translate(-50%, -50%);
-        transform    : translate(-50%, -50%);
-    }
-
-    p {
-        display: inline-block;
-    }
-
-    .landing-image {
-        width: 100%;
-        border-radius: 12px;
-    }
-
-    .landing-image:hover {
-        box-shadow: 20px 20px 50px 15px grey;
-    }
-</style>
-
 <div class="grid-center v-align-m">
-    <div class="col-12">
-        <h1>Welcome.</h1>
-    </div>
-    <div class="col_sm-12">
-        <a href="/fineart"><img class="landing-image" src="/view/__image/landing-fineart.jpg" /></a>
-    </div>
-    <div class="col_sm-12">
-        <a target="_jmportraits" href="//jmportraits.com"><img class="landing-image" src="/view/__image/landing-portraits.jpg" /></a>
+    <div class="col">
+
+        <div class="grid-center">
+            <div class="col-12">
+                <h1>Welcome.</h1>
+            </div>
+        </div>
+
+        <div class="grid-center">
+            <div class="col_sm-12">
+                <a href="/fineart"><img class="landing-image" src="/view/__image/landing-fineart.jpg" /></a>
+            </div>
+            <div class="col_sm-12">
+                <a target="_jmportraits" href="//jmportraits.com"><img class="landing-image" src="/view/__image/landing-portraits.jpg" /></a>
+            </div>
+        </div>
+
     </div>
 </div>
