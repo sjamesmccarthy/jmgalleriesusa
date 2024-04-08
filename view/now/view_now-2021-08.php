c95ecdd2b73380cababcbb1d0c7a03ae1fe2a8cf
James McCarthy <james@jmgalleries.com>
2024-04-08 12:43:25 -0700

    changes

---
+++ view/now/view_now-2021-08.php
@@ -0,0 +1,58 @@
+<section id="now">
+    
+    <div class="grid-center">
+        <div class="col-8_sm-11">    
+    
+                <h3 class="text-center">What Am I Doing Now?</h3>
+                    <p class="nosiderule text-center">
+                     This is a <a target="_new" href="https://nownownow.com/about"/>now</a> page, inspired by<a target="_new" href="https://sive.rs/nowff"> Derek Sivers</a>, you should make one too.
+                    </p>
+                    <p class="nosiderule text-center"><a target="_new" href="/fieldnotes">my Fieldnotes</a> | <a target="_new" href="//vlog.jmgalleries.com">my YouTube</a></p>
+                    
+                <h3>Hot August Nights</strike></h3>
+                    <p>
+                    Old cars and 100 degree weather are here for 2021. It officially kicked off last weekend (August 1) in Virginia City, Nevada and will end on Sunday August 8. If it wasn't so smokey again I probably would have taken the short trip up to Reno and done a little old-car shooting and maybe found a food truck to have lunch at. However, with AQIs reaching in the upper 100s the air quickly becomes pretty miserable to breathe. But I was able to still catch a few of the classic cars in VC and am now looking forward to the Great Reno Balloon Race next month as well as cleaning air and blue skies.
+                    </p>
+                    
+                    
+                <h3>Photography and Fine-Art ...</h3>
+                    <p>
+                    This is going to sound like a broken record, but again, the smokey air is being a real nuisance this summer for my lungs and my lens. Last weekend (first weekend of August) we had a break and took a short trip up the Tahoe where we hiked to Monkey Rock. I got a few keepers on the trip which you can <a target="_new" href="https://instagram.com/jmgalleriesusa">see on my Instagram</a> and picked one for my <a href="/oceans-lakes-waterfalls/big-sky">August New Release</a>. View it over at my online gallery: <a href="/">jmgalleries.com</a>. 
+                    <br /><br />
+                    I have also kicked off a new playlist on my YouTube channel called "Thoughts While Drinking Coffee...". They are short, 3 minutes or less, videos of me talking about photography things. <a target="_new" href="https://vlog.jmgalleries.com">Check them out on YouTube</a>. I upload to this playlist every other week right now and on the alternate weeks usually upload a vlog and product demo. You can help by subscribing!
+                    <br /><br />
+                    Hopefully by the end of the month I will finally be accepting pre-orders for my first photo-book, Untitled. Follow me on <a target="_new" href="https://twitter.com/jmgalleriesusa">Twitter</a> or <a target="_new" href="https://vlog.jmgalleries.com">YouTube</a> to stay up to date on when that exciting bit of news will officially drop.
+                    <br /><br />
+                    Otherwise I also started a monthly giveaway through my <a href="/fieldnotes/first-friday-new-release-email-give-away">First Friday New Release Email</a> where one lucky subscriber will win an 8x10 print matted to 11x14 of the month's new release. All you have to do is subscribe to the once-a-month email and be a USA resident to be eligible. 
+                    </p>
+                    
+                <h3>Reading ...</h3>
+                    <p>
+                    Still reading the sequel to "Ready, Player One" back in January. I have made some progress and hope to finish it soon, but honestly it's not as engaging as the first book and taking some real energy. 
+                    </p>
+                    
+                <h3>Backyard ...</h3>
+                    <p>
+                    The month of July was extremely slow going because of, yep, you guessed it, the smoke. Working outside in 95 degree weather is tough enough, but add an unhealthy AQI of 160+ and it's just down-right miserable, so Michele and I took a break. Hopefully this month will improve and we will be able to enjoy our new paver patio before winter. As progress is made pics will come, or links to pics will come. Fingers crossed AQI improves and those wildfires burning nearby us (Dixie, Tammarack and River) are finally contained. 
+                    </p>
+                    
+                <h3>Baseball ...</h3>
+                    <p>
+                    For the first time in a month, my Red Sox have fallen out of first place. Excited to see Chris Sale back on the mound this coming weekend (8/14) and hope to see our guys start hitting again and reclaim first place!
+                    </p>
+                
+                <p class="mt-32 nosiderule text-center">Created on  Sun, 08  Aug 2021
+                    <br />Previously on /now: <a href="/now/2021/july">July</a> | <a href="/now/2021/august">August</a> | <a href="/now/2021/august">September</a>
+                    <br />
+                    <a target="_new" class="mr-16 blue" style="font-size: 1.5rem" href="http://twitter.com/jmgalleriesusa"><i class="fab fa-twitter"></i></a>
+                </p>
+        </div>
+        
+        <div class="col-8_sm-11 newsletter-section-fn" style="text-align: center;">
+            <h3 class="nomar-top">Sign Up For My First Friday Email</h3>
+            <?php $this->getPartial('newsletter'); ?>
+        </div>
+        
+    </div>
+
+</section>
---
+++ view/now/view_now.php
@@ -0,0 +1,28 @@
+<section id="now">
+
+    <div class="grid-center">
+        <div class="col-8_sm-11">
+
+                <h3 class="text-center">What Am I Doing Now?</h3>
+<!--                     <p class="nosiderule text-center" style="font-size: 1.5rem;"><a class="mr-16" target="_new" href="/fieldnotes"><i class="fab fa-twitter"></i></a> <a target="_new" href="//vlog.jmgalleries.com"><i class="fab fa-youtube"></i></a></p> -->
+
+                <div class="mt-32"></div>
+
+                <div id="December-21" class="month">
+                </div>
+
+
+                <div id="social" class="mt-32">
+                    <p class="nosiderule text-center tiny">
+                     This is a <a target="_new" href="https://nownownow.com/about"/>now</a> page, inspired by<a target="_new" href="https://sive.rs/nowff"> Derek Sivers</a>, you should make one too.
+                    </p>
+
+                    <br />Previously on /now: <a href="/now/2021/july">July</a> | <a href="/now/2021/august">August</a> | <a href="/now/2021/september">September</a> | <a href="/now/2021/october">October</a>
+
+                </div>
+        </div>
+
+
+    </div>
+
+</section>
